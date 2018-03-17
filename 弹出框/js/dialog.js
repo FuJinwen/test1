a8d4d4a37724098f2add32286189619e47b30cea
FuJinwen <1243629655@qq.com>
2018-03-17 16:33:32 +0800

    弹出框

--- 弹出框/js/dialog.js
+++ 弹出框/js/dialog.js
@@ -1,9 +1,15 @@
 /**
  * Created by dell on 2018/3/17.
  */
-define(function () {
+require.config({
+    paths:{
+        jquery:'jquery-3.3.1'
+    }
+});
+define(["jquery"],function ($) {
     var dialog={
         open:function () {
+
             var dialogHtml =
             '<div class="dialog-box">'+
                 '<div class="dialog-mask"></div>'+
@@ -11,15 +17,20 @@ define(function () {
                 '<div class="dialog-header">'+
                 '<div class="dialog-title">注册</div>'+
                 '<div class="dialog-btn" id="close">[X]</div>'+
+                // '<div class="dialog-btn" id="close2">[X]</div>'+
                 '</div>'+
                 '<div class="dialog-content"></div>'+
                 '</div>'+
                 '</div>';
-            document.body.innerHTML+=dialogHtml;
-            var oClose = document.getElementById("close");
-            oClose.onclick = function () {
-                document.getElementsByClassName("dialog-box")[0].remove();
-            }
+            // document.body.innerHTML+=dialogHtml;
+            // var oClose = document.getElementById("close");
+            // oClose.onclick = function () {
+            //     document.getElementsByClassName("dialog-box")[0].remove();
+            $("body").append(dialogHtml);
+            $("#close").click ( function () {
+                $(".dialog-box").remove();
+            })
+
         }
     };
     return dialog;
--- 弹出框/js/index.js
+++ 弹出框/js/index.js
@@ -1,6 +1,12 @@
-require(["dialog"],function (dialog) {
-    var oBtn = document.getElementById("btn")
-    oBtn.onclick = function () {
-        dialog.open();
+require.config({
+    paths:{
+        jquery:'jquery-3.3.1'
     }
+});
+define(["dialog","jquery"],function (dialog,$) {
+    // var oBtn = document.getElementById("btn")
+    $("#btn").click( function () {
+        console.log("1");
+        dialog.open();
+    })
 });
