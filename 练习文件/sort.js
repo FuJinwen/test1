d022e463465cda32f92d860ddd3ee84b244bc9c5
FuJinwen <1243629655@qq.com>
2018-03-17 15:12:25 +0800

    弹出框

---
+++ 练习文件/sort.js
@@ -0,0 +1,10 @@
+define(function () {
+    function sortArr(arr) {
+        return arr.sort(function (a,b) {
+            return b-a;
+        });
+    }
+    return sortArr;
+});/**
+ * Created by dell on 2018/3/17.
+ */
