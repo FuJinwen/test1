6d37235955417f15f4eac23b6152eacf7c16f0fc
FuJinwen <1243629655@qq.com>
2018-03-18 09:02:02 +0800

    弹出框

--- 练习文件/b.js
+++ 练习文件/b.js
@@ -1,5 +1,10 @@
-require(["sort"],function (sortArr) {
-    var arr = [5,8,6,1,3];
-    var arr1 = sortArr(arr);
-    console.log(arr1);
+// require(["sort"],function (sortArr) {
+//     var arr = [5,8,6,1,3];
+//     var arr1 = sortArr(arr);
+//     console.log(arr1);
+// });
+define(function (require) {
+    var  arr = [5,8,6,3,1];
+    var sortArr = require("sort");
+    console.log(sortArr(arr));
 });
