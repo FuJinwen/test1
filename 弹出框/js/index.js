529791543fc398a6eb62c743241efb0f73fc20dc
FuJinwen <1243629655@qq.com>
2018-03-18 10:28:32 +0800

    弹出框

--- 弹出框/js/index.js
+++ 弹出框/js/index.js
@@ -1,12 +1,31 @@
-require.config({
-    paths:{
-        jquery:'jquery-3.3.1'
+requirejs.config({
+    paths: {
+        jquery: 'jquery-3.3.1'
     }
 });
-define(["dialog","jquery"],function (dialog,$) {
-    // var oBtn = document.getElementById("btn")
-    $("#btn").click( function () {
-        console.log("1");
-        dialog.open();
+require(["dialog","jquery"],function (Dialog,$) {
+    // var oBtn = $("#btn");
+    // oBtn.onclick = function () {
+    //     dialog.open();
+    // }
+    var settings = {
+        height:500,
+        title:"登录",
+        content:"a.html"
+    };
+    var settings1 = {
+        height:300,
+        title:"登录",
+        content:"登录成功"
+    }
+    $("#btn1").click(function () {
+        var dialog1= new Dialog();
+        dialog1.open(settings);
+    });
+    $("#btn").click(function () {
+        var dialog= new Dialog();
+        dialog.open(settings1);
     })
-});
+    //AMD
+    //CMD
+})
